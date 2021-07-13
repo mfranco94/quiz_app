17fc639d7343ba62a3a87af5f224fe8e8e9295e6
Marcos Franco <developer.mfranco@gmail.com>
2021-07-13 15:51:52 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizApp());
+
+class QuizApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello, flutter!'),
+    );
+  }
+}
