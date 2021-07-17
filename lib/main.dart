ca42e0c6525952f3287bfc567bd0f63c00774f33
Marcos Franco <developer.mfranco@gmail.com>
2021-07-16 21:46:54 -0300

    add question array, question text and 3 button

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,32 @@ void main() => runApp(QuizApp());
 
 class QuizApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final questions = [
+      "'What's your favorite color?",
+      "What's your favorite pet?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz'),
         ),
-        body: Text('Hello, flutter!'),
+        body: Column(
+          children: [
+            Text(questions[0]),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
